1594882d4d83ae1f19f4f232ebb836bc13ebd299
yasintrm <yasintrm@gmail.com>
2012-10-04 22:20:12 -0700

    json

--- log.js
+++ log.js
@@ -21,6 +21,7 @@ var LOGGER = (function () {
                 return this.log('error: ' + arg + ' line: ' + line, true, true);
             },
             log: function (arg, printToConsole, err) {
+				arg = JSON.stringify(arg);
                 div = doc.createElement('div');
                 style = div.style;
                 style.font = '12px Tahoma';
